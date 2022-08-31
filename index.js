006a1d9776c4e0d1f275950cc4781a4aef3fdaae
Sam Avery <sam.avery1@gmail.com>
2022-08-31 16:36:51 -0500

    commit tree d786be86968a449bfe62354ac2ba1d8fff080985

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// function superbowlWin(record){
+//     result === 'W';
+//     return (record.year)
+// }
+
+// const superbowlWin = record.find(result => result === 'W')
+
+// ['year: "2015", result: "w"'].find(superbowlWin);
+// ['year: "2014", result: "N/A"'].find(superbowlWin);
+// ['year: "2013", result: "L"'].find(superbowlWin);
+
+
+// function superbowlWin(record){
+//     record.find(record.result === 'W')
+//     return record.year 
+// }
+
+
+// function superbowlWin(record){
+// for (let item of record){
+//     if(item.result === "W")
+//     {return item.year}
+// }} 
+
+superbowlWin = (record) => {
+    const result = record.find(element => element.result === "W")
+    return result ? (result.year) : undefined 
+}
